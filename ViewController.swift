982f5f931ace805a1346f39886cc1cdf01654607
gdsanders <gd@digitalequity.com>
2015-08-07 18:32:49 -0500

    Playing with UIColor

--- ViewController.swift
+++ ViewController.swift
@@ -10,15 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "sasha.jpg")!)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func redButtonTouched(sender: UIButton) {
+        view.backgroundColor = UIColor.redColor()
+    }
+    
+    @IBAction func greenButtonTouched(sender: UIButton) {
+        view.backgroundColor = UIColor.greenColor()
+    }
+    
+    @IBAction func blueButtonTouched(sender: UIButton) {
+        view.backgroundColor = UIColor(hue: 240/360, saturation: 0.4, brightness: 0.7, alpha: 1.0)
+    }
+    
+    
 
 
 }
